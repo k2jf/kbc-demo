86427db6e916fa48e64aed0df12bddff22ab5659
wangliheng <wangliheng@k2data.com.cn>
2019-04-23 11:05:03 +0800

    Squashed 'src/main/java/com/k2data/kbc/login/' content from commit 2d3ec26
    git-subtree-dir: src/main/java/com/k2data/kbc/login
    git-subtree-split: 2d3ec26f1d56a63992db417a218fcdc217273942

---
+++ config/WebConfigurer.java
@@ -0,0 +1,26 @@
+package com.k2data.kbc.login.config;
+
+import com.k2data.kbc.login.config.intercepors.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor).addPathPatterns("/**").excludePathPatterns("/login", "/register");
+    }
+
+}
