46b7290d603fbddf0cc7ced03afa830ea99d1ab9
zhanghao <1891934@qq.com>
2019-04-29 18:53:12 +0800

    kbc_announce表改名为t_announce

--- init_db.sql
+++ init_db.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `kbc_announce`;
-CREATE TABLE `kbc_announce` (
+DROP TABLE IF EXISTS `t_announce`;
+CREATE TABLE `t_announce` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '物理主键',
   `content` varchar(255) NOT NULL COMMENT '通知正文内容',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
@@ -10,9 +10,9 @@ CREATE TABLE `kbc_announce` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
-LOCK TABLES `kbc_announce` WRITE;
-INSERT INTO `kbc_announce` VALUES (1,'通知1','remark1','2019-01-01 00:00:00','2019-01-02 00:00:00','zhangsan','2019-01-01 00:00:00');
-INSERT INTO `kbc_announce` VALUES (2,'通知2','remark2','2019-01-01 00:00:00','2019-01-02 00:00:00','zhangsan','2019-01-01 00:00:00');
-INSERT INTO `kbc_announce` VALUES (3,'通知3','remark3','2019-01-01 00:00:00','2019-01-02 00:00:00','zhangsan','2019-01-01 00:00:00');
-INSERT INTO `kbc_announce` VALUES (4,'通知4','remark4','2019-01-01 00:00:00','2019-01-02 00:00:00','zhangsan','2019-01-01 00:00:00');
+LOCK TABLES `t_announce` WRITE;
+INSERT INTO `t_announce` VALUES (1,'通知1','remark1','2019-01-01 00:00:00','2019-01-02 00:00:00','zhangsan','2019-01-01 00:00:00');
+INSERT INTO `t_announce` VALUES (2,'通知2','remark2','2019-01-01 00:00:00','2019-01-02 00:00:00','zhangsan','2019-01-01 00:00:00');
+INSERT INTO `t_announce` VALUES (3,'通知3','remark3','2019-01-01 00:00:00','2019-01-02 00:00:00','zhangsan','2019-01-01 00:00:00');
+INSERT INTO `t_announce` VALUES (4,'通知4','remark4','2019-01-01 00:00:00','2019-01-02 00:00:00','zhangsan','2019-01-01 00:00:00');
 UNLOCK TABLES;
