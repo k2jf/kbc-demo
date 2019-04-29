18b7a6ec703149e5d5336a22561e113ac701adaa
wangliheng <wangliheng@k2data.com.cn>
2019-04-29 19:00:50 +0800

    Squashed 'src/main/java/com/k2data/kbc/login/' content from commit 1339a52
    git-subtree-dir: src/main/java/com/k2data/kbc/login
    git-subtree-split: 1339a52f2a2cedf24ae2a72a9f00c86902dafdad

---
+++ README.md
@@ -0,0 +1,30 @@
+# kbc-auth-login
+
+## 功能
+用户验证
+
+## 维护者
+wangliheng@k2data.com.cn
+
+
+## git拉取示例
+```
+1. git remote add -f kbc-auth-login git@github.com:k2jf/kbc-auth-login.git
+
+2. git subtree add -P src/main/java/com/k2data/kbc/auth/login kbc-auth-login master --squash
+```
+
+##说明
+```
+1. 不拦截路径："/login"、"/register"
+2. 登录接口
+
+```
+请求参数
+
+| 属性        | 说明     | 类型   | 默认值 |
+| ----------- | -------- | ------ | ------ |
+| userName | 用户名 | String | Null | 
+| password | 密码 | String | Null |
+
+http://localhost:9080/login?userName=admin&password=admin
---
+++ config/intercepors/LoginInterceptor.java
@@ -0,0 +1,33 @@
+package com.k2data.kbc.auth.login.config.intercepors;
+
+import com.k2data.kbc.api.KbcBizException;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+
+        Boolean validFlag = (Boolean) session.getAttribute("validFlag");
+        if (validFlag == null || validFlag == false){
+            throw new KbcBizException("用户未登录");
+            //return false;
+        }else {
+            return true;
+        }
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
