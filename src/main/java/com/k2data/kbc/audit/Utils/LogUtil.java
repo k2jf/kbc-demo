e29bc59e11e66ba3dcbb5260a339270332faf61a
yuda <yuda@k2data.com.cn>
2019-05-06 16:06:30 +0800

    去除异常记录里

--- src/main/java/com/k2data/kbc/audit/Utils/LogUtil.java
+++ src/main/java/com/k2data/kbc/audit/Utils/LogUtil.java
@@ -1,24 +1,18 @@
 package com.k2data.kbc.audit.Utils;
 
 import com.k2data.kbc.audit.dao.AuditLogMapper;
-import com.k2data.kbc.audit.dao.AuditExceptionLogMapper;
 import com.k2data.kbc.audit.model.AuditLog;
-import com.k2data.kbc.audit.model.AuditExceptionLog;
 
 public class LogUtil {
 
 
     private static AuditLogMapper auditLogDao = SpringContextHolderUtil.getBean(AuditLogMapper.class);
 
-    private static AuditExceptionLogMapper exLogDao = SpringContextHolderUtil.getBean(AuditExceptionLogMapper.class);
 
     public static void saveLog(AuditLog log) {
         new SaveLogRunnable(log).run();
     }
 
-    public static void saveExLogRunnable(AuditExceptionLog exlog) {
-        new SaveExLogRunnable(exlog).run();
-    }
 
     public static class SaveLogRunnable implements Runnable {
 
@@ -35,17 +29,4 @@ public class LogUtil {
         }
     }
 
-    public static class SaveExLogRunnable implements Runnable {
-
-        private AuditExceptionLog exLog;
-
-        public SaveExLogRunnable(AuditExceptionLog exLog) {
-            this.exLog = exLog;
-        }
-
-        @Override
-        public void run() {
-            exLogDao.insert(exLog);
-        }
-    }
 }
